cbc34c9a5ee1eb59221894f468798262b34fee89
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-08 15:00:49 +0000

    updated destroy wf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,15 @@ export default async function Home() {
   return (
     <main className="main">
       <div>
-        <h2>Hello World</h2>
+        <h2>Hello Ross</h2>
         <p>This is a test</p>
         <p>This is also a test</p>
       </div>
       <Link href="/about">About</Link>
       <br />
-      {flags.isFeatureEnabled("search") && <input placeholder="Search" />}
+      {flags.isFeatureEnabled("search") && (
+        <input placeholder="You're a twat" />
+      )}
     </main>
   );
 }
