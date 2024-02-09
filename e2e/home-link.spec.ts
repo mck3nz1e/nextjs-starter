ab8e9913b2460bf60683e524963bb29a46a9d287
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 19:30:04 +0000

    fixed broken test

--- e2e/home-link.spec.ts
+++ e2e/home-link.spec.ts
@@ -3,19 +3,12 @@ import { test, expect } from "@playwright/test";
 test.describe("App Home Page", () => {
   test.beforeEach(async ({ page }) => {
     const awsEnvCname = process.env.aws_env_cname;
-
-    console.log("!!!!!!!!!");
-    console.log(awsEnvCname);
-    console.log("!!!!!!!!!");
-
     const isDeployed = process.env.aws_env_cname !== undefined; // Check if the variable is defined
 
     if (isDeployed) {
-      console.log(`Testing in deployed environment: ${awsEnvCname}`);
       let url = awsEnvCname as string;
       await page.goto(url);
     } else {
-      console.log(`Testing in local environment`);
       await page.goto("http://localhost:3000");
     }
   });
@@ -40,7 +33,7 @@ test.describe("App Home Page", () => {
     await page.click("text=About");
 
     // The new URL should be "/about" (baseURL is used there)
-    await expect(page).toHaveURL("http://localhost:3000/about");
+    await expect(page.url()).toContain("/about");
 
     // The new page should contain an h2 with "About Page"
     await expect(page.locator("h2")).toContainText("About");
