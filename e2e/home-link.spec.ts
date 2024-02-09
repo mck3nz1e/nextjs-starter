ed2ece0048a311e47b49696f7bec2d678a3f1c1d
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 19:11:53 +0000

    another bug fix

--- e2e/home-link.spec.ts
+++ e2e/home-link.spec.ts
@@ -2,18 +2,22 @@ import { test, expect } from "@playwright/test";
 
 test.describe("App Home Page", () => {
   test.beforeEach(async ({ page }) => {
-    const awsEnvCname = process.env._aws_env_cname as string;
+    const awsEnvCname = process.env._aws_env_cname;
+
+    console.log("!!!!!!!!!");
+    console.log(awsEnvCname);
+    console.log("!!!!!!!!!");
+
     const isDeployed = process.env.aws_env_cname !== undefined; // Check if the variable is defined
-    let url: string;
 
     if (isDeployed) {
       console.log(`Testing in deployed environment: ${awsEnvCname}`);
-      url = awsEnvCname;
+      let url = awsEnvCname as string;
+      await page.goto(url);
     } else {
       console.log(`Testing in local environment`);
-      url = "http://localhost:3000";
+      await page.goto("http://localhost:3000");
     }
-    await page.goto(url);
   });
 
   test.afterEach(async ({ page }, testInfo) => {
