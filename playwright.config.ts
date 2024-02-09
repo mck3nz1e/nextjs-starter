53e1bd3643066f675f4e8beaa1ddb0d224a4b11c
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 20:53:24 +0000

    updated report name for final time

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "@playwright/test";
 const isCI = process.env.CI == "true";
 const isDeployed = process.env.aws_env_cname;
-const date = new Date();
 
 export default defineConfig({
   testDir: "./e2e",
-  reporter: [
-    [
-      "html",
-      {
-        open: "never",
-        outputFolder: `playwright-report/report-${date[Symbol.toPrimitive](
-          "number",
-        )}`,
-      },
-    ],
-  ],
+  reporter: [["html", { open: "never" }]],
 
   webServer:
     isCI && isDeployed === undefined
