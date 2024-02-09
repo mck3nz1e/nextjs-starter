6771ec367e32954c870ba3c51ed80576fa4b7d5e
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 17:33:20 +0000

    possiblke fix for ci tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from "@playwright/test";
 const isCI = process.env.CI === "true";
+const isDeployed = process.env.aws_env_cname === "true";
 
 export default defineConfig({
   testDir: "./e2e",
   reporter: [["html", { open: "never" }]],
 
-  webServer: isCI
-    ? undefined
-    : {
-        command: "npm run start",
-        url: "http://localhost:3000",
-      },
+  webServer:
+    (isCI && isDeployed) === undefined
+      ? {
+          command: "npm run start",
+          url: "http://localhost:3000",
+        }
+      : undefined,
 });
