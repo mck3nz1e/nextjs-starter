71e2692bebcfea1f66688a4b7d281d75df440073
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 17:49:02 +0000

    fix for config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "@playwright/test";
-const isCI = process.env.CI === "true";
-const isDeployed = process.env.aws_env_cname === "true";
+const isCI = process.env.CI == "true";
+const isDeployed = process.env.aws_env_cname;
+console.log(isDeployed);
+console.log(isCI);
 
 export default defineConfig({
   testDir: "./e2e",
   reporter: [["html", { open: "never" }]],
 
   webServer:
-    (isCI && isDeployed) === undefined
+    isCI && isDeployed === undefined
       ? {
           command: "npm run start",
           url: "http://localhost:3000",
