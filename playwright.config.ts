a992eb972c8826961cf89821881fb3626b1409c2
Scott McKenzie <scottmckenzie09@googlemail.com>
2024-02-09 17:19:17 +0000

    added check for tests running in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "@playwright/test";
+const isCI = process.env.CI === "true";
 
 export default defineConfig({
   testDir: "./e2e",
   reporter: [["html", { open: "never" }]],
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:3000",
-  },
+
+  webServer: isCI
+    ? undefined
+    : {
+        command: "npm run start",
+        url: "http://localhost:3000",
+      },
 });
